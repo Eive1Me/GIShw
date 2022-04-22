a18292ce05fddbb320c4d14862edf372dacf0988
milesst <79859505+milesst@users.noreply.github.com>
2022-04-22 18:46:45 +0300

    added save to database (not finished) 2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,24 +2,36 @@ package sample;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import sample.databasemanage.entity.Map;
+import sample.databasemanage.repo.CoordinateRepo;
+import sample.databasemanage.repo.MapObjectRepo;
+import sample.databasemanage.repo.MapRepo;
+import sample.databasemanage.repo.RadiusRepo;
+import sample.entities.MapObject;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
 
-public class Controller {
+public class Controller implements Initializable {
 
     @FXML
     AnchorPane pane;
@@ -32,24 +44,70 @@ public class Controller {
     File file;
     BufferedImage img;
 
+    //map data
+    ArrayList<MapObject> mapObjects;
+    Integer startShirota = 5;
+    Integer startDolgota = 5;
+    Integer endShirota = 0;
+    Integer endDolgota = 0;
+    private Double startX = 0.0;
+    private Double startY = 0.0;
+    private Double endX = 0.0;
+    private Double endY = 0.0;
+
+    //repo
+    CoordinateRepo coordinateRepo;
+    MapRepo mapRepo;
+    MapObjectRepo mapObjectRepo;
+    RadiusRepo radiusRepo;
+
     public void openF(){
         fileChooser.setTitle("Choose your picture");
         Stage stage = new Stage();
         file = fileChooser.showOpenDialog(stage);
         try {
             try {
+                //create new map with image from filechooser
                 img = ImageIO.read(new FileInputStream(file));
                 imgView.setImage(null);
                 imgView.setImage(SwingFXUtils.toFXImage(img,null));
+
+                //creating list of objects
+                mapObjects = new ArrayList<>();
             } catch (NullPointerException ignored){}
         } catch (IOException ignored){}
     }
 
-    public boolean saveF(){
+    public boolean saveF() throws IOException {
+        String saveChoice = "toDatabase";
+
+        switch (saveChoice) {
+            case "toDatabase": saveToDatabase(); break;
+            case "toDevice": saveToDevice(); break;
+        }
         return true;
     }
 
-    public void closeF(){
+
+    public boolean saveToDatabase() throws IOException {
+        mapObjects.add(new MapObject("obj", "descr", new Circle(4, 10, 10), 1));
+        //save to database as map
+        //save as new map
+        Map map = new Map(1, startShirota, startDolgota, endShirota, endDolgota, startX, startY, endX, endY, file);
+        map = mapRepo.insert(map);
+        for (MapObject object: mapObjects) {
+            sample.databasemanage.entity.MapObject obj = object.toDbEntity(object, map.getId());
+            obj = mapObjectRepo.insert(obj);
+        }
+        return true;
+    }
+
+    public boolean saveToDevice() {
+        //save to device as picture
+        return true;
+    }
+
+    public void closeF() throws IOException {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Закрытие карты");
         alert.setHeaderText("Сохранить карту?");
@@ -74,6 +132,19 @@ public class Controller {
         coordsLbl.setText(new Coords(e.getSceneX(), e.getSceneY()-25).toString());
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            coordinateRepo = new CoordinateRepo();
+            mapObjectRepo = new MapObjectRepo();
+            radiusRepo = new RadiusRepo();
+            mapRepo = new MapRepo();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
     public static class Coords{
         Double x;
         Double y;
@@ -88,4 +159,5 @@ public class Controller {
             return x + "; " + y;
         }
     }
+
 }
